024fadd3980535d9a3bd920b74ebc20355360464
antoshka838 <anivanov1987788990012@gmail.com>
2025-05-10 00:16:37 +0300

    change dark and light theme, add theme switch

--- script.js
+++ script.js
@@ -314,7 +314,7 @@ function getReviewsWord(count) {
     if (count % 10 === 1 && count % 100 !== 11) {
         return 'отзыва';
     } else if ([2, 3, 4].includes(count % 10) && ![12, 13, 14].includes(count % 100)) {
-        return 'отзыва';
+        return 'отзывов';
     }
     return 'отзывов';
 }
